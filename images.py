bcc8afa617ebb1903d7826138cc9de0f707137ca
UnrivalledHitman120703 <indrajeetmondal120703@gmail.com>
2023-10-17 22:14:37 +0530

    Some more funcitons in Pillow

--- images.py
+++ images.py
@@ -1,3 +1,7 @@
+# Pillow library for image manioulation
+# Name = Indrajeet Mondal; Date = 17th October 2023
+# SourceCode
+
 from PIL import Image, ImageFilter
 
 # Open an image
@@ -7,20 +11,52 @@ img = Image.open("pikachu.jpg")
 filtered_img = img.filter(ImageFilter.BLUR)
 filtered_img.save("blur.png", "png")
 
+# Re-open the original image
+img = Image.open("pikachu.jpg")  # You need to re-open the original image
+
 # Applying the image filter: Smooth
 filtered_img = img.filter(ImageFilter.SMOOTH)
 filtered_img.save("smooth.png", "png")
 
+# Re-open the original image again
+img = Image.open("pikachu.jpg")
+
 # Applying the image filter: Sharpen
 filtered_img = img.filter(ImageFilter.SHARPEN)
 filtered_img.save("sharpen.png", "png")
+
+# Re-open the original image again
+img = Image.open("pikachu.jpg")
+
+# Rotating the image
+crooked = img.rotate(270)  # Rotate the original image
+crooked.save("rotate.png", "png")
+
+# Re-open the original image again
+img = Image.open("pikachu.jpg")
+
+# Resizing the image
+resize = img.resize((300, 300))
+resize.save("resized.png", "png")
+
+# Re-open the original image again
+img = Image.open("pikachu.jpg")
+
+# Selecting a region
+# The crop method takes a box argument, so you need to pass it as a tuple (left, upper, right, lower)
+region = img.crop((100, 100, 400, 400))
+region.save("region.png", "png")
+
+# Re-open the original image again
+img = Image.open("pikachu.jpg")
 
 # Converting the image
 filtered_img = img.convert("L")
 filtered_img.save("grey.png", "png")
 
 # Display the image
-print(img)
+filtered_img.show()
+
 # Display image properties
 print(img.format)
 print(img.mode)
