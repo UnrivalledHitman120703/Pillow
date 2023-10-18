3ffe19c6f6adf59cf795023f275b113af1c1a73f
UnrivalledHitman120703 <indrajeetmondal120703@gmail.com>
2023-10-19 01:22:30 +0530

    Debugging JPGtoPNGconverter.py completed

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -1,3 +1,7 @@
+# Converting jpg to png using pillow library
+# Name = Indrajeet Mondal; Date = 19th October 2023
+# SourceCode
+
 import sys
 import os
 from PIL import Image
@@ -11,8 +15,7 @@ if not os.path.exists(directory):
 
 for filename in os.listdir(path):
   clean_name = os.path.splitext(filename)[0]
-  img = Image.open(f'{path}{filename}')
-  #added the / in case user doesn't enter it. You may want to check for this and add or remover it. 
+  img = Image.open(f'{path}{filename}') 
   img.save(f'{directory}/{clean_name}.png', 'png')
   print('all done!!')
 
