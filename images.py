2f3ea91e3dcf30f0653c8a0c720f8544f7965eba
UnrivalledHitman120703 <indrajeetmondal120703@gmail.com>
2023-10-18 21:10:17 +0530

    Moved the images to Pokedex folder

--- images.py
+++ images.py
@@ -5,54 +5,54 @@
 from PIL import Image, ImageFilter
 
 # Open an image
-img = Image.open("pikachu.jpg")
+img = Image.open("Pokedex/pikachu.jpg")
 
 # Applying the image filter: Blur
 filtered_img = img.filter(ImageFilter.BLUR)
-filtered_img.save("blur.png", "png")
+filtered_img.save("Pokedex/blur.png", "png")
 
 # Re-open the original image
-img = Image.open("pikachu.jpg") 
+img = Image.open("Pokedex/pikachu.jpg") 
 
 # Applying the image filter: Smooth
 filtered_img = img.filter(ImageFilter.SMOOTH)
-filtered_img.save("smooth.png", "png")
+filtered_img.save("Pokedex/smooth.png", "png")
 
 # Re-open the original image again
-img = Image.open("pikachu.jpg")
+img = Image.open("Pokedex/pikachu.jpg")
 
 # Applying the image filter: Sharpen
 filtered_img = img.filter(ImageFilter.SHARPEN)
-filtered_img.save("sharpen.png", "png")
+filtered_img.save("Pokedex/sharpen.png", "png")
 
 # Re-open the original image again
-img = Image.open("pikachu.jpg")
+img = Image.open("Pokedex/pikachu.jpg")
 
 # Rotating the image
 crooked = img.rotate(270) 
-crooked.save("rotate.png", "png")
+crooked.save("Pokedex/rotate.png", "png")
 
 # Re-open the original image again
-img = Image.open("pikachu.jpg")
+img = Image.open("Pokedex/pikachu.jpg")
 
 # Resizing the image
 resize = img.resize((300, 300))
-resize.save("resized.png", "png")
+resize.save("Pokedex/resized.png", "png")
 
 # Re-open the original image again
-img = Image.open("pikachu.jpg")
+img = Image.open("Pokedex/pikachu.jpg")
 
 # Selecting a region
 # The crop method takes a box argument, so you need to pass it as a tuple (left, upper, right, lower)
 region = img.crop((100, 100, 400, 400))
-region.save("region.png", "png")
+region.save("Pokedex/region.png", "png")
 
 # Re-open the original image again
-img = Image.open("pikachu.jpg")
+img = Image.open("Pokedex/pikachu.jpg")
 
 # Converting the image
 filtered_img = img.convert("L")
-filtered_img.save("grey.png", "png")
+filtered_img.save("Pokedex/grey.png", "png")
 
 # Display the image
 filtered_img.show()
